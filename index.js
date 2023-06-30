2b6bbdfe702b7783b61691d053542500754022e3
wamtitu <wamtitujosegmail.com>
2023-06-30 16:44:39 +0300

    fixed cors

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:5173'
-}));
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
